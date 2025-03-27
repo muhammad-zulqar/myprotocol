38c338cced10b4dacf628e697386331f7e9094eb
muhammad-zulqar <khanmuhammadzulqarnain34@gmail.com>
2025-03-27 15:43:46 +0530

    Add files via upload

---
+++ protocol.js
@@ -0,0 +1,19 @@
+const profile = {
+    name: 'Nikola Tesla',
+    born: 1856,
+    died: 1943,
+    contribution: 'Alternating current (AC)',
+    patents: 300,
+    quotes: [
+        'If you want to find the secrets of the universe, think in terms of energy, frequency and vibration.',
+        'The present is theirs; the future, for which I really worked, is mine.',
+        'The scientists of today think deeply instead of clearly. One must be sane to think clearly, but one can think deeply and be quite insane.',
+        'The day science begins to study non-physical phenomena, it will make more progress in one decade than in all the previous centuries of its existence.',
+        'My brain is only a receiver, in the Universe there is a core from which we obtain knowledge, strength and inspiration. I have not penetrated into the secrets of this core, but I know that it exists.',
+    ],
+    books: [
+        'My Inventions: The Autobiography of Nikola Tesla',
+        'The Problem of Increasing Human Energy',
+        'The Inventions, Researches, and Writings of Nikola Tesla',
+    ],
+}
